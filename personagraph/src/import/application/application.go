b8b7884e52eac17c59fec4c5989de618148cb5f3
Evgeniy Shurmin <eshurmin@gmail.com>
2018-03-13 14:38:39 +0300

    add -s flag to set aerospike hosts

--- personagraph/src/import/application/application.go
+++ personagraph/src/import/application/application.go
@@ -3,6 +3,7 @@ package application
 import (
 	"import/config"
 	"github.com/sirupsen/logrus"
+	"github.com/aerospike/aerospike-client-go"
 )
 
 type Application struct {
@@ -12,11 +13,14 @@ type Application struct {
 
 func New(config *config.Config, logger *logrus.Entry) (*Application, error) {
 	application := &Application{Config: config, Logger: logger}
+	application.Logger.Infof("Aerospike hosts: %s", config.Hosts.String())
+	aerospike.NewClientWithPolicyAndHost(aerospike.NewClientPolicy(), config.Hosts.Aerospike()...)
 	return application, nil
 }
 
 func (self *Application) Run() error {
 	for _, name := range self.Config.Files {
+		self.Logger.Info("..")
 		if err := self.Import(name, self.Logger.WithField("file", name)); err != nil {
 			return err
 		}
--- personagraph/src/import/config/config.go
+++ personagraph/src/import/config/config.go
@@ -4,16 +4,80 @@ import (
 	"flag"
 	"os"
 	"fmt"
+	"strings"
+	"github.com/aerospike/aerospike-client-go"
+	"net"
+	"strconv"
 )
 
+type Host struct {
+	Address string
+	Port    int
+}
+
+func (self *Host) String() string {
+	return fmt.Sprintf("%s:%d", self.Address, self.Port)
+}
+
+type Hosts []Host
+
+func (self *Hosts) String() string {
+	hosts := make([]string, 0)
+
+	for _, host := range *self {
+		hosts = append(hosts, host.String())
+	}
+
+	return strings.Join(hosts, ", ")
+}
+
+func (self *Hosts) Aerospike() []*aerospike.Host {
+	hosts := make([]*aerospike.Host,0)
+
+	for _, host := range *self {
+		hosts = append(hosts, &aerospike.Host{
+			Name: host.Address,
+			Port: host.Port,
+		})
+	}
+	return hosts
+}
+
+func (self *Hosts) Set(value string) error {
+	address, port, err := net.SplitHostPort(value)
+
+	if err != nil {
+		fmt.Printf("Invalid aerospike address(%v): %s\n", err, value)
+		os.Exit(1)
+	}
+
+	host := Host{Address: address}
+
+	if port == "" {
+		host.Port = 3000
+	} else if p, err := strconv.Atoi(port); err != nil {
+		fmt.Printf("Invalid aerospike port(%v): %s\n", err, port)
+		os.Exit(1)
+	} else {
+		host.Port = p
+	}
+
+	*self = append(*self, host)
+	return nil
+}
+
 type Config struct {
-	Files  []string
-	Remove bool
+	Files     []string
+	Remove    bool
+	Hosts     Hosts
+	Importers int
 }
 
 func New() (*Config, error) {
 	config := &Config{
-		Remove: false,
+		Remove:    false,
+		Hosts:     Hosts{},
+		Importers: 1,
 	}
 
 	options := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
@@ -21,8 +85,17 @@ func New() (*Config, error) {
 		fmt.Fprintf(os.Stderr, "Usage of %s: [options] <files>\n", os.Args[0])
 		options.PrintDefaults()
 	}
+
+	options.Var(&config.Hosts, "s", "Aerospike hosts")
+
 	options.BoolVar(&config.Remove, "r", config.Remove, "remove file after processing")
+	options.IntVar(&config.Importers, "i", config.Importers, "number of parallel importers")
 	options.Parse(os.Args[1:])
+
+	if len(config.Hosts) == 0 {
+		config.Hosts = Hosts{Host{"127.0.0.1",3000}}
+	}
+
 	config.Files = options.Args()
 
 	return config, nil
